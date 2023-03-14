9313790173b4e072f348f2dcffe33ff5e3e4101c
nikit <nikit@DESKTOP-F29JDOU>
2023-03-14 16:27:36 +0300

    add in GitHub

---
+++ p2p1/p2p1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2p1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum = a + b;
+            int diff = sum - b;
+            int mult = a * b;
+            int div = a / b;
+            Console.WriteLine(sum + " " + diff + " " + mult + " " + div);
+        }
+    }
+}
